60e9787ab584606b0c4e72ac563f3f20b14c4043
vittxrio <60070720+vittxrio@users.noreply.github.com>
2020-01-19 17:49:43 +0100

    Update script.js

--- nativeuilut/scripts/script.js
+++ nativeuilut/scripts/script.js
@@ -1,80 +1,23 @@
-
-// Load in the required modules
-const NativeUI = require('NativeUI');
+const Materials = require('Materials');
 const Scene = require('Scene');
+const NativeUI = require('NativeUI');
 const Textures = require('Textures');
-const patch = require('Patches');
-const d = require('Diagnostics');
-const t = require('Time');
-var rectname = patch.getStringValue('rectname');
-var toticon = patch.getScalarValue('toticon');
-// var rectname = "rectangle";
-//  var toticon = 2;
+const Patches = require('Patches');
+const TouchGestures = require('TouchGestures');
 
-d.log(toticon.pinLastValue() + " icon");
-d.log(rectname.pinLastValue() + " rect");
+const index = 0;
+const configuration = {
+  selectedIndex: index,
+  items: [
+    {image_texture: Textures.get('IMG_1067')},
+{image_texture: Textures.get('anime 2')},
+{image_texture: Textures.get('anime 3')},
+  ],
+};
+const picker = NativeUI.picker;
+picker.configure(configuration);
+picker.visible = true;
 
-t.setTimeout(() => {
-
-
-  for (var i = 0; i <= toticon.pinLastValue() - 1; i++) {
-    if (i == 0) {
-      Scene.root.find(rectname.pinLastValue() + "0").hidden = false;
-    }
-    else {
-      Scene.root.find(rectname.pinLastValue() + i).hidden = true;
-    }
-  }
-  const picker = NativeUI.picker;
-
-  // Set a starting index (optional, will be 0 by default)
-  const index = 0;
-
-  // Create a configuration object
-  const configuration = {
-
-    // The index of the selected item in the picker
-    selectedIndex: index,
-
-    // The image textures to use as the items in the picker
-    items: [
-
-      // {image_texture: Textures.get('icon0')},
-      // {image_texture:Textures.get('icon1')}
-
-    ]
-
-  };
-
-  for (var i = 0; i <= toticon.pinLastValue() - 1; i++) {
-    d.log(i)
-    configuration['items'].push({ image_texture: Textures.get('icon' + i) });
-
-  }
-  d.log(configuration["items"])
-  //==============================================================================
-  // Apply the configuration and show the picker
-  //==============================================================================
-
-  // Configure the picker using the configuration object
-  picker.configure(configuration);
-
-  // Set the picker to be visible (visible is false by default)
-  picker.visible = true;
-
-  //==============================================================================
-  // Use the picker to set the texture of the plane
-  //==============================================================================
-
-  // Subscribe to the selectedIndex scalar signal
-  picker.selectedIndex.monitor().subscribe(function (index) {
-    for (var i = 0; i <= toticon.pinLastValue() - 1; i++) {
-      if (i == index.newValue) {
-        Scene.root.find(rectname.pinLastValue() + index.newValue).hidden = false;
-      }
-      else {
-        Scene.root.find(rectname.pinLastValue() + i).hidden = true;
-      }
-    }
-  });
-}, 500);
+picker.selectedIndex.monitor().subscribe(function(val) {
+  Patches.setScalarValue("Number", val.newValue);
+});
